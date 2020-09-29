9bc1b6b3ac9ec1acc9c12f5204ab18ce5712b08d
Georgios Mpovolis <gam154@scarletmail.rutgers.edu>
2020-09-29 13:33:10 -0400

    updated functionality of pages with updated components

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,13 +1,33 @@
 // put together profile components
-
 import React from "react";
-
+import { Col, Container, Row } from "../components/Grid";
+import API from "../utils/API";
 
 function Profile(){
+
+    // const create = function(req, res) {
+    //     db.CreateUser
+    //         .create(req.body)
+    //         .then(dbModel => res.json(dbModel))
+    //         .catch(err => res.status(422).json(err));
+    // }
+
+    // const findById = function(req, res) {
+    //     db.CreateUser  
+    //         .findById(req.params.id)
+    //         .then(dbModel => res.json(dbModel))
+    //         .catch(err => res.status(422).json(err))
+    // }
+
+
     return (
-        <div>
-            <h3>Profile</h3>
-        </div>
+        <Container>
+            <Row>
+                <Col size="md-8">
+
+                </Col>
+            </Row>
+        </Container>
     )
 }
 
