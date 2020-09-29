43c245fcda13a2d3c301b817ece71ea1c3b489f7
Georgios Mpovolis <gam154@scarletmail.rutgers.edu>
2020-09-29 13:23:49 -0400

    updated folder structure

---
+++ config/passport/GoogleStrategy.js
@@ -0,0 +1,43 @@
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20");
+const keys = require("../keys");
+const {GoogleUser} = require("../../models");
+// const SignUpUser = require("../models/create-user");
+
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+    GoogleUser.findById(id).then((user) => {
+        done(null, user);
+    });
+});
+
+passport.use(new GoogleStrategy({
+    // options for the google strat
+    callbackURL: "/auth/google/redirect",
+    clientID: keys.google.clientID,
+    clientSecret: keys.google.clientSecret
+}, (accessToken, refreshToken, profile, done) => {
+    // passport cb function
+    //first check if user already exists in our db
+    console.log("this is google profile: ", profile);
+    GoogleUser.findOne({googleId: profile.id}).then((currentUser) => {
+        if(currentUser){
+            // already have the user
+            console.log("this is currentUser inside passport cb if check: ", currentUser);
+            done(null, currentUser);
+        } else{
+            //if not, create user in our db
+            new GoogleUser({
+                username: profile.displayName,
+                googleId: profile.id
+            }).save().then((newUser) => {
+                console.log("newUser created in mongo from passport cb function: ", newUser);
+                done(null, newUser);
+            })
+        }
+    })
+})
+)
