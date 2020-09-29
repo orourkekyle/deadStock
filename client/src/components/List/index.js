b9ce86168d97510e0e450c68a869aca3bc3aa977
Georgios Mpovolis <gam154@scarletmail.rutgers.edu>
2020-09-29 13:32:07 -0400

    created and updated styling and functionality of components

--- client/src/components/List/index.js
+++ client/src/components/List/index.js
@@ -1,32 +1,45 @@
 import React from "react";
 
-export function List({ children }) {
-    return (
-        <div className="list-overflow-container">
-            <ul className="list img-list">
-                {children}
-            </ul>
-        </div>
-    );
-}
+// export function List({ children }) {
+//     return (
+//         <div className="list-overflow-container">
+//             <ul className="list img-list">
+//                 {children}
+//             </ul>
+//         </div>
+//     );
+// }
 
-export function ListItem({ children }) {
-    return (
-        <li>
-            <div className="list-img">
-                {children.image}
-            </div>
-            <div className="li-text">
-                <div className="li-head">
-                    {children.name}
-                </div>
-                <div className="li-sub">
-                    {children.brand}
-                </div>
-                <div className="li-sub">
-                    {children.releaseYear}
-                </div>
-            </div>
-        </li>
+// export function ListItem({ children }) {
+//     console.log("children", children);
+//     return (
+//         <li>
+//             <div className="list-img">
+//                 {children.image}
+//             </div>
+//             <div className="li-text">
+//                 <div className="li-head">
+//                     {children.shoe}
+//                 </div>
+//                 <div className="li-sub">
+//                     {children.brand}
+//                 </div>
+//                 <div className="li-sub">
+//                     {children.gender}
+//                 </div>
+//                 <div className="li-sub">
+//                     {children.year}
+//                 </div>
+//             </div>
+//         </li>
+//     );
+    export const List = ({ children }) => (
+        <ul className="list-group">
+        {children}
+        </ul>
     );
-}
+
+    export function ListItem({ children }) {
+        return <li className="list-group-item">{children}</li>;
+      }
+//}
