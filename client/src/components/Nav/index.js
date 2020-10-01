7758169a669ec0b163f9cc859d4665d47732dd0f
zerobit <orourke_kyle@icloud.com>
2020-10-01 12:29:59 -0400

    fixed nav component to be up to date and relevent to our project

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,61 +1,14 @@
 import React from 'react';
-import { Col, Row } from "../Grid";
 import './style.css';
-// import { render } from 'react-dom';
-import { Nav } from 'rsuite';
-
-// const styles = { width: 100, position: 'relative', zIndex: '2' };
-
-const CustomNav = ({ active, onSelect, ...props }) => {
+// import Navbar from 'react-bootstrap/Navbar';
+function Nav() {
   return (
-    <Nav {...props} vertical activeKey={active} onSelect={onSelect} style={{position: 'relative', zIndex: '2', width: 100}}>
-      <Nav.Item eventKey="home">
-        Home
-      </Nav.Item>
-      <Nav.Item eventKey="news">News</Nav.Item>
-      <Nav.Item eventKey="solutions">Solutions</Nav.Item>
-      <Nav.Item eventKey="products">Products</Nav.Item>
-      <Nav.Item eventKey="about">About</Nav.Item>
-    </Nav>
-  );
-};
-
-class Demo extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      active: 'home'
-    };
-    this.handleSelect = this.handleSelect.bind(this);
-  }
-  handleSelect(activeKey) {
-    this.setState({ active: activeKey });
-  }
-  render() {
-    const { active } = this.state;
-    return (
-      <Row>
-        <Col md={4}>
-          <CustomNav active={active} onSelect={this.handleSelect} />
-        </Col>
-
-        <Col md={6}>
-          <CustomNav appearance="tabs" active={active} onSelect={this.handleSelect} />
-        </Col>
-        <Col md={4}>
-          <CustomNav appearance="tabs" reversed active={active} onSelect={this.handleSelect} />
-        </Col>
-        <Col md={6}>
-          <CustomNav appearance="subtle" active={active} onSelect={this.handleSelect} />
-        </Col>
-        <Col md={4}>
-          <CustomNav appearance="subtle" reversed active={active} onSelect={this.handleSelect} />
-        </Col>
-      </Row>
-    );
-  }
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <a className="navbar-nav" href="/">Login</a>
+      <a className="navbar-nav" href="/signup">Sign up</a>
+      <a className="navbar-nav" href="/browsing">Browsing</a>
+      <a className="navbar-nav" href="/profile">Profile</a>
+    </nav>
+  )
 }
-
-// ReactDOM.render(<Demo />);
-
-export default Demo;
+export default Nav;
