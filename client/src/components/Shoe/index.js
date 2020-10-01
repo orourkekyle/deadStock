729fa82a542c1fce1cbb4a1182f039037aa7ad1d
Thomas Mullaney <tloring.mullaney@gmail.com>
2020-10-01 14:38:16 -0700

    browsing js almost done with css

--- client/src/components/Shoe/index.js
+++ client/src/components/Shoe/index.js
@@ -7,14 +7,14 @@ function Shoe({ shoe, brand, gender, year, image, colorway, price,  Button}) {
     return (
         <div>
         {/* <React.Fragment> */}
-            <Card className="border border-success">
+            <Card style={{width: '18rem'}} className="box border border-success">
                 <Card.Body >
                     <Card.Title>{shoe}</Card.Title>
                     <Card.Text>Brand: {brand}</Card.Text>
                     <Card.Text>Gender: {gender} Year: {year}</Card.Text>
                     <Card.Text>Colorway: {colorway}</Card.Text>
                     <Card.Text>Retail Price: {price}</Card.Text>
-                    <Card.Img src={image}  alt="shoe" />
+                    <Card.Img style={{maxHeight: '250px', maxWidth: '250'}}src={image}  alt="shoe" />
                     <Button />
                 </Card.Body>
             </Card>
