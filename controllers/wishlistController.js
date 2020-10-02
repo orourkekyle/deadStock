1f7f902ff2ed50594b04f69561a5a37d3d5587d8
zerobit <orourke_kyle@icloud.com>
2020-10-02 15:26:04 -0400

    fixed server set up, got oauth working by implementing url route directly into hrefs using variables, started connecting wishlist collection and googleuser collection

--- controllers/wishlistController.js
+++ controllers/wishlistController.js
@@ -2,7 +2,7 @@ const db = require("../models");
 
 module.exports = {
     findAll: function(req, res) {
-        console.log("req.query: ", req.query);
+        console.log("req.query - inside wishlistController: ", req.query);
         db.Wishlist.find(req.query)
             .then(dbWishlist => {
                 console.log("dbWishlist - inside wishlistController findAll: ", dbWishlist);
@@ -11,13 +11,13 @@ module.exports = {
             .catch(err => res.status(422).json(err));
     },
 
-    create: function(req, res) {
-        console.log("req.body - inside wishlistController create: ", req.body)
+    create: function({ body }, res) {
+        console.log("body - inside wishlistController create: ", body)
         db.Wishlist
-            .create(req.body)
-            .then(dbWishlist => {
-                console.log("dbWishlist - inside wishlistController create: ", dbWishlist);
-                return res.json(dbWishlist)
+            .create(body)
+            .then(({ googleId }) => db.GoogleUser.findOneAndUpdate({}, { $push: { sneakers: googleId } }, { new: true }))
+            .then(dbGoogleUser => {
+                res.json(dbGoogleUser);
             })
             .catch(err => res.status(422).json(err))
     },
@@ -27,6 +27,15 @@ module.exports = {
             .findById(req.params.sneakerId)
             .then(dbWishlist => res.json(dbWishlist))
             .catch(err => res.status(422).json(err))
+    },
+
+    find: function(req, res) {
+        db.GoogleUser.find({})
+            .populate("sneakers")
+            .then(dbGoogleUser => {
+                res.json(dbGoogleUser);
+            })
+            .catch(err => res.status(422).json(err));
     },
 
     remove: function(req, res) {
